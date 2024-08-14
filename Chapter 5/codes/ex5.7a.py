588354866f6304b51bdeb1ee95fb7710070609eb
Weng20011103 <144203566+Weng20011103@users.noreply.github.com>
2024-08-15 00:08:10 +0800

    add the python file to plot example 5.7

---
+++ Chapter 5/codes/ex5.7a.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the unit step function
+def unit_step(x):
+    return np.where(x >= 0, 1, 0)
+
+def f(x, ω):
+  return unit_step(x+ω)-unit_step(x-ω)
+
+x = np.linspace(-3*np.pi, 3*np.pi, 20000)
+
+# Create a figure with a specific size (width, height) in inches
+plt.figure(figsize=(12, 2))
+
+# Plot the function
+plt.plot(x, f(x+2*np.pi, 1)+f(x, 1)+f(x-2*np.pi, 1))
+
+# Customize x-axis ticks and labels
+x_ticks = [-3*np.pi, -2*np.pi, -np.pi, -1, 0, 1, np.pi, 2*np.pi, 3*np.pi]
+x_labels = ['$-3\pi$', '$-2\pi$', '$-\pi$', '$-\omega_c$', '0', '$\omega_c$','$\pi$', '$2\pi$', '$\omega$']
+plt.xticks(x_ticks, x_labels)
+
+# Customize y-axis ticks and labels
+y_ticks = [0, 1]
+y_labels = ['0', '1']
+plt.yticks(y_ticks, y_labels)
+
+# Set a title for the plot
+plt.title('$H_{lp}(e^{j\omega})$')
+
+# Add x = 0 and y = 0 axes
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
+
+plt.xlabel('t', loc='right')
+
+# Display the plot
+plt.show()
